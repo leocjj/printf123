a52ae5f6b867c78e01e86d13ab3fbbfa0c593b67
leocjj <leocj@hotmail.com>
2019-10-28 06:35:12 +0000

    printf v06

--- _printf.c
+++ _printf.c
@@ -26,14 +26,9 @@ int _printf(const char *format, ...)
 	{
 		if (*(format + i) == '%')
 		{
-			/*if (*(format + i + 1) == '%')
-				concat_c(buffer, '%', &chars_printed);*/
 			count_conversion = count_id(format + i);
 			if (count_conversion == 0)
-			{
-				/*concat_c(buffer, '%', &chars_printed);*/
 				continue;
-			}
 			switch (*(format + i + count_conversion))
 			{
 			case 'c':
